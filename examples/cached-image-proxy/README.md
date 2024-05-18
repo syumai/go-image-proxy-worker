dfe30253ebc9506b4c679196ac1104a7d6d4e5bc
syumai <syumai@gmail.com>
2024-05-18 13:20:55 +0900

    add cached-image-proxy example

---
+++ examples/cached-image-proxy/README.md
@@ -0,0 +1,31 @@
+# cached-image-proxy
+
+- An example worker which caches response from go-image-proxy-worker.
+
+## Example
+
+https://cached-image-proxy.syumai.com/?file=curry.jpg&size=500&format=jpeg
+
+### Available options
+
+* file
+  - syumai.png (small PNG)
+  - curry.jpg (middle-sized JPEG)
+  - akiba.jpg (large JPEG)
+* size
+  - number
+* format
+  - jpeg
+  - gif
+  - tiff
+
+## Development
+
+* At first, please run `npm i`.
+
+### Commands
+
+```
+npm run dev    # run dev server
+npm run deploy # deploy worker
+```
---
+++ examples/cached-image-proxy/src/index.ts
@@ -0,0 +1,63 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { cache } from "hono/cache";
+import { etag } from "hono/etag";
+import { z } from "zod";
+import type { ImageProxy, ParseOptions } from "../../../dist";
+
+type Bindings = {
+  IMAGE_PROXY: Service<ImageProxy>;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+const schema = z.object({
+  file: z.union([
+    z.literal("syumai.png"),
+    z.literal("curry.jpg"),
+    z.literal("akiba.jpg"),
+  ]),
+  size: z.preprocess((x) => Number(x), z.number()).optional(),
+  format: z
+    .union([z.literal("jpeg"), z.literal("png"), z.literal("tiff")])
+    .optional(),
+});
+
+app.get(
+  "/",
+  zValidator("query", schema, (result, c) => {
+    if (!result.success) {
+      return c.text("Bad Request", 400);
+    }
+  }),
+  etag({ weak: true }),
+  cache({
+    cacheName: "cached-image-proxy",
+    cacheControl: "public, max-age=14400",
+  }),
+  async (c) => {
+    const data = c.req.valid("query");
+
+    const opts: ParseOptions = {};
+    if (data.size !== undefined) {
+      opts.size = data.size;
+      opts.fit = true;
+    }
+    if (data.format !== undefined) {
+      opts.format = data.format;
+    }
+    const proxyRes = await c.env.IMAGE_PROXY.proxy(
+      new URL(c.req.url).origin,
+      `https://r2-image-viewer.syumai.com/${data.file}`,
+      opts
+    );
+
+    c.header(
+      "Content-Type",
+      proxyRes.headers.get("Content-Type") ?? "application/octet-stream"
+    );
+    return c.body(proxyRes.body);
+  }
+);
+
+export default app;
